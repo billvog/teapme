66305e224a8bb845bcbb3f2027756cccde5fa013
billvog <billvog04@gmail.com>
2024-09-20 16:34:52 +0300

    removed main tag from loading component 😬

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
 import { Loader2 } from "lucide-react";
 
-export default function Login() {
+export default function Loading() {
   return (
-    <main className="flex h-full w-full flex-1 flex-col items-center justify-center gap-10">
+    <div className="flex h-full w-full flex-1 flex-col items-center justify-center gap-10">
       <Loader2 size={32} className="animate-spin text-gray-400" />
-    </main>
+    </div>
   );
 }
